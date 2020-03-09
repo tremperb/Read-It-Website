22894ad8deb4e3ccc1a2226d218fa9f768de7da0
tremperb <55967770+tremperb@users.noreply.github.com>
2020-03-09 16:59:44 -0700

    Node website initial

---
+++ server.js
@@ -0,0 +1,58 @@
+/*
+ * Write your routing code in this file.  Make sure to add your name and
+ * @oregonstate.edu email address below.
+ *
+ * Name: Brayden Tremper
+ * Email: tremperb@oregonstate.edu
+ */
+
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars'); //so handlebars works
+var app = express();
+app.set('port', process.argv[2]);
+
+/////
+var mysql = require('./dbcon.js');
+var bodyParser = require('body-parser');
+//var pool = mysql.createPool(require('./dbcon.js'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+////
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+app.set('mysql', mysql);
+
+/////////////////////
+var express = require('express');
+var mysql = require('./dbcon.js');
+var bodyParser = require('body-parser');
+
+//////////////////////
+
+//No need to edit this one
+app.get('/', function (req, res, next) {
+  res.status(200).render('mainPage');
+});
+
+//Change style to app.use when set up like others
+app.get('/bookTrip', function(req, res, next) {
+  res.status(200).render('bookTrip');
+});
+
+app.use('/participants', require('./participants.js'));
+app.use('/tripInfo', require('./tripInfo.js'));
+app.use('/admin', require('./admin.js'));
+
+
+
+app.get('*', function (req, res) {
+  res.status(404).render('error404');
+});
+
+app.listen(app.get('port'), function () {
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});

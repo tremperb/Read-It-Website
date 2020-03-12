784f3484f6c6ca64ba2742c0a9d269f413c2de25
tremperb <55967770+tremperb@users.noreply.github.com>
2020-03-11 18:15:28 -0700

    Added font change

--- public/index.js
+++ public/index.js
@@ -10,3 +10,27 @@ function updateMenu(uID) {
 
   return false;
 }
+
+
+function updateFont() {
+
+  var selectedFont = document.getElementById("fontOne").value;
+  console.log("Selected Font: ", selectedFont)
+  var item = document.getElementById("output-text");
+  if(selectedFont == "Open Dyslexic") {
+    item.style.fontFamily = "open-dyslexic";
+  }
+  else {
+    item.style.fontFamily = "comic-sans";
+  }
+}
+
+$(document).ready(function() {
+
+  var elementExists = document.getElementById("output-text");
+  if(elementExists) {
+    updateFont();
+    //call other functions here
+  }
+
+});
